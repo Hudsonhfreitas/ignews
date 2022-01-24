5bd96353107255ca70fb6f9c2250b83ef74b13de
Hudson Freitas <hudson223@gmail.com>
2022-01-24 10:44:48 -0300

    feat: adding sign in button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,18 @@
 import styles from '../Header/styles.module.scss'
+import { SignInButton } from '../SignInButton'
 
 export function Header() {
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
+                <nav>
+                    <a className={styles.active} href="">Home</a>
+                    <a href="">Posts</a>
+                </nav>
+            
+                <SignInButton />
             </div>
-            <nav>
-                <a href="">Home</a>
-                <a href="">Posts</a>
-            </nav>
         </header>
     )
 }
